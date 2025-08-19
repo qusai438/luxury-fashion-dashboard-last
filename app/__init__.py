6cf4c60a5b21201789dddbca70acb08866a546b0
qusai438 <qsay634@gmail.com>
2025-08-19 10:35:40 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .routes import bp as core_bp
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
     app.register_blueprint(core_bp)
     return app
